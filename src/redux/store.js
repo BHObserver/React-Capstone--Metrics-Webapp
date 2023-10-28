535f219fdd7df8a80c8f73dc6e57cb816ec42a76
BHObserver <BHObserver0@gmail.com>
2023-10-28 18:44:52 +0600

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import countryReducer from './country/allCountriesSlice';
+import airPollutionReducer from './airPollution/airPollutionSlice'; // Import the air pollution reducer
+import cityReducer from './country/geoSlice'
+
+const store = configureStore({
+  reducer: {
+    countries: countryReducer,
+    city: cityReducer,
+    airPollution: airPollutionReducer,
+  },
+});
+
+export default store;
