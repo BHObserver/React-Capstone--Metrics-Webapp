1ef45a5e5d7093cd11809a13ef94819e72eaf427
BHObserver <BHObserver0@gmail.com>
2023-10-28 18:44:48 +0600

    Create airPollutionSlice.js

---
+++ src/redux/airPollution/airPollutionSlice.js
@@ -0,0 +1,39 @@
+// airPollutionSlice.js
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchAirPollutionData = createAsyncThunk(
+  'airPollution/fetchAirPollutionData',
+  async ({ lat, lon, apiKey }) => {
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      );
+      return response.data;
+    } catch (error) {
+      throw error(error.message);
+    }
+  }
+);
+
+const airPollutionSlice = createSlice({
+  name: 'airPollution',
+  initialState: {
+    data: null,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAirPollutionData.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchAirPollutionData.rejected, (state, action) => {
+        state.data = null;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default airPollutionSlice.reducer;
