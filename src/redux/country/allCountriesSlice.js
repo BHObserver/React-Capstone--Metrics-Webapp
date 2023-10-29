2fc8cc5d5830a268264e023726bdce73e3330cb2
BHObserver <BHObserver0@gmail.com>
2023-10-29 23:37:11 +0600

    Update allCountriesSlice.js

--- src/redux/country/allCountriesSlice.js
+++ src/redux/country/allCountriesSlice.js
@@ -8,15 +8,7 @@ export const initialState = {
   error: '',
 };
 
-export const allCountries = createAsyncThunk('country/allCountries', async () => {
-  try {
-    const response = await axios.get('https://restcountries.com/v3.1/all');
-    const { data } = response;
-    return data;
-  } catch (error) {
-    throw error(error.message);
-  }
-});
+
 
 export const searchRegion = createAsyncThunk('country/searchRegion', async (region) => {
   try {
